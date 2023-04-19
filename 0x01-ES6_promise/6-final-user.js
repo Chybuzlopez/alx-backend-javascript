15e7255e2941a7d4dd2b20cc1c69ccac12391e23
Chybuzlopez <danielorihanna@gmail.com>
2023-04-19 20:58:26 +0000

    task done

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,17 @@
 /* eslint-disable import/named */
 // eslint-disable-next-line import/named
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import { signUpUser } from './4-user-promise.js';
+import { uploadPhoto } from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => result.map((res) => (
-      {
-        status: res.status,
-        value: res.status === 'fulfilled' ? res.value : res.reason,
+    .then(results => {
+      const [userResult, photoResult] = results;
+      if (userResult.status === 'fulfilled' && photoResult.status === 'fulfilled') {
+        return [{ success: true }, { url: photoResult.value }];
+      } else {
+        throw new Error('One or more promises rejected');
       }
-    )));
+    });
 }
+
