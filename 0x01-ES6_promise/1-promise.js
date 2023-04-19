ad7fb7329061130e814591d2b26de7b115d1235b
Chybuzlopez <danielorihanna@gmail.com>
2023-04-19 19:13:17 +0000

    task done

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,5 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => { // eslint-disable-next-line no-constant-condition
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
