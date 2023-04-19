df982e4b9046182a3cdb1d4f53d11463e45258ae
Chybuzlopez <danielorihanna@gmail.com>
2023-04-19 19:49:18 +0000

    task done

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,4 @@
-export default function handleResponseFromAPI(promise) {
+export function handleResponseFromAPI(promise) {
   promise
     .then((response) => {
       console.log('Got a response from the API');
